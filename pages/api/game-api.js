0716286c654972ff7d3832d735d03ada1969584c
Nathan's here <94665436+NathanKanaeru@users.noreply.github.com>
2024-10-20 01:35:41 +0700

    Create game-api.js

---
+++ pages/api/game-api.js
@@ -0,0 +1,17 @@
+// pages/api/game-api.js
+export default function handler(req, res) {
+  res.status(200).json({
+    clientVersionCode: 1,
+    clientSampVersionCode: 1,
+    gameFilesVersionCode: 1,
+    URL_GAME_FILES: "https://www.mediafire.com/file/f21fgy9vu6vk7sq/game.zip/file",
+    URL_GAME_FILES_UPD: "http://45.130.141.87/files_upd.zip",
+    URL_VK: "https://vk.com/",
+    URL_DISCORD: "https://discord.gg/",
+    URL_YOUTUBE: "",
+    URL_FORUM: "",
+    URL_DONATE: "",
+    URL_CLIENT: "http://45.130.141.87/launcher.apk",
+    URL_CLIENTSAMPAPK: "http://45.130.141.87/client.apk"
+  });
+}
