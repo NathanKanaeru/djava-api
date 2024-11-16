5cb3e7c55e67e9d80a7c6a05c81307e1fdbc49d9
NathanKanaeru <kanaerunathan@gmail.com>
2024-11-16 11:49:12 +0700

    updated

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,9 +1,21 @@
 // components/Navbar.js
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Navbar() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Cek status mode dari localStorage saat komponen dimuat
+    const darkModePreference = localStorage.getItem('darkMode');
+    if (darkModePreference === 'true') {
+      setIsDarkMode(true);
+      document.body.classList.add('bg-gray-900', 'text-white');
+    } else {
+      setIsDarkMode(false);
+      document.body.classList.remove('bg-gray-900', 'text-white');
+    }
+  }, []);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -11,8 +23,12 @@ function Navbar() {
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-    document.body.classList.toggle('bg-gray-900', !isDarkMode);
-    document.body.classList.toggle('text-white', !isDarkMode);
+    localStorage.setItem('darkMode', !isDarkMode); // Simpan status mode di localStorage
+    if (!isDarkMode) {
+      document.body.classList.add('bg-gray-900', 'text-white');
+    } else {
+      document.body.classList.remove('bg-gray-900', 'text-white');
+    }
   };
 
   return (
@@ -56,7 +72,7 @@ function Navbar() {
       </div>
 
       {/* Modal */}
-      {isModalOpen && (
+      { isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white rounded-lg p-6 shadow-lg w-11/12 md:w-1/3">
             <h2 className="text-xl font-bold mb-4">Menu</h2>
