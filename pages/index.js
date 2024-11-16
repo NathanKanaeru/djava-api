d6adc0b7523ec378a5b057c5f31945918ffef8f6
NathanKanaeru <kanaerunathan@gmail.com>
2024-11-16 10:26:48 +0700

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Head from 'next/head';
+// pages/_app.js
+import '../styles/globals.css'; // Ensure this line is present
 
+import Head from 'next/head';
 export default function Home() {
   return (
     <>
